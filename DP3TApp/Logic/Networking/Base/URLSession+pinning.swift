e76e18d7f45c84b2eed67a74837126e59a7b5feb
Fabian Aggeler <aggeler@ubique.ch>
2020-05-19 00:45:54 +0200

    Add certificate pinning for TEST environment

--- DP3TApp/Logic/Networking/Base/URLSession+pinning.swift
+++ DP3TApp/Logic/Networking/Base/URLSession+pinning.swift
@@ -43,9 +43,11 @@ class CertificateEvaluator: NSObject, URLSessionDelegate {
         let hosts = ["www.pt1.bfs.admin.ch",
                      "www.pt1-d.bfs.admin.ch",
                      "www.pt1-a.bfs.admin.ch",
+                     "www.pt1-t.bfs.admin.ch",
                      "codegen-service.bag.admin.ch",
                      "codegen-service-d.bag.admin.ch",
-                     "codegen-service-a.bag.admin.ch"]
+                     "codegen-service-a.bag.admin.ch",
+                     "codegen-service-t.bag.admin.ch"]
         for host in hosts {
             if let certificate = bundle.getCertificate(with: host) {
                 let evaluator = UBPinnedCertificatesTrustEvaluator(certificates: [certificate], validateHost: true)
@@ -58,6 +60,7 @@ class CertificateEvaluator: NSObject, URLSessionDelegate {
             let evaluator = UBPinnedCertificatesTrustEvaluator(certificates: [c], validateHost: true)
             evaluators["www.pt-d.bfs.admin.ch"] = evaluator
             evaluators["www.pt-a.bfs.admin.ch"] = evaluator
+            evaluators["www.pt-t.bfs.admin.ch"] = evaluator
             evaluators["www.pt.bfs.admin.ch"] = evaluator
         }
 
