b71bd716ef5402d412eefacd3c1db963d33e9704
Alexandru Lazar <alexandru.lazar@cgm.com>
2020-05-29 13:14:37 +0300

    Added ViewModel for NSMeldungenDetailNoMeldungenViewController

---
+++ DP3TApp/Screens/Meldungen/MeldungenDetailNoMeldungenViewModel.swift
@@ -0,0 +1,49 @@
+///
+
+import Foundation
+
+class MeldungenDetailNoMeldungenViewModel {
+    
+    var simpleModuleTitleText: String {
+        return "no_meldungen_box_title".ub_localized
+    }
+    
+    var simpleModuleSubtitleText: String {
+        return "no_meldungen_box_subtitle".ub_localized
+    }
+    
+    var simpleModuleText: String {
+        return "no_meldungen_box_text".ub_localized
+    }
+    
+    var externalLinkButtonTitleText: String {
+        return "no_meldungen_box_link".ub_localized
+    }
+    
+    // Stack View OnboardingInfoView
+    
+    
+    var firstOnboardingInfoText: String {
+        return "meldungen_nomeldungen_faq1_text".ub_localized
+    }
+    
+    var firstOnboardingInfoTitle: String {
+        return "meldungen_nomeldungen_faq1_title".ub_localized
+    }
+    
+    var secondOnboardingInfoText: String {
+        return "meldungen_nomeldungen_faq2_text".ub_localized
+    }
+    
+    var secondOnboardingInfoTitle: String {
+        return "meldungen_nomeldungen_faq2_titel".ub_localized
+    }
+    
+    var externalLinkURL: URL? {
+        return URL(string: "no_meldungen_box_url".ub_localized)
+    }
+    
+    init() {
+        
+    }
+}
