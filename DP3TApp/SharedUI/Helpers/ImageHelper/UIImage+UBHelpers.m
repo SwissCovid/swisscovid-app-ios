b1f62e290782e51b21ff2dd7e2a625b70fb91487
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2020-05-29 16:47:57 +0200

    updates copyright header

--- DP3TApp/SharedUI/Helpers/ImageHelper/UIImage+UBHelpers.m
+++ DP3TApp/SharedUI/Helpers/ImageHelper/UIImage+UBHelpers.m
@@ -1,10 +1,12 @@
-//
-//  UIImage+UBHelpers.m
-//  mainlib
-//
-//  Created by Fabian Aggeler on 10/9/13.
-//  Copyright (c) 2013 Ubique. All rights reserved.
-//
+/*
+ * Copyright (c) 2020 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
 
 #import "UIImage+UBHelpers.h"
 
