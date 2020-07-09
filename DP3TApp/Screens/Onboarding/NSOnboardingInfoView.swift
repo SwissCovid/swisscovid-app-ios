2043a6b896aa017a5604c5a0dd4c7ee8df83a0fa
Bastian Morath <morath@ubique.ch>
2020-07-09 14:30:41 +0200

    groups text of information views together when voiceover is enabled

--- DP3TApp/Screens/Onboarding/NSOnboardingInfoView.swift
+++ DP3TApp/Screens/Onboarding/NSOnboardingInfoView.swift
@@ -69,6 +69,9 @@ class NSOnboardingInfoView: UIView {
             make.trailing.equalToSuperview().inset(leftRightInset)
             make.bottom.equalToSuperview().inset(NSPadding.medium)
         }
+
+        isAccessibilityElement = true
+        accessibilityLabel = ( title ?? " ") + text
     }
 
     required init?(coder _: NSCoder) {
