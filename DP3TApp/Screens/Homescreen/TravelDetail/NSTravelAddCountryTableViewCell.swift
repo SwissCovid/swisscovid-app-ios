3787b6ef4948406ccbdcf683f4f2315f0eb82d14
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-08-24 11:23:41 +0200

    updates divider spacing

--- DP3TApp/Screens/Homescreen/TravelDetail/NSTravelAddCountryTableViewCell.swift
+++ DP3TApp/Screens/Homescreen/TravelDetail/NSTravelAddCountryTableViewCell.swift
@@ -85,7 +85,7 @@ class NSTravelAddCountryTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         if let view = contentView.superview {
-            bottomSeparator.frame = CGRect(x: NSPadding.large, y: view.bounds.height - 1, width: view.bounds.width - 2 * NSPadding.large, height: 1)
+            bottomSeparator.frame = CGRect(x: NSPadding.large, y: view.bounds.height - 1, width: view.bounds.width - NSPadding.large - 10, height: 1)
         }
 
         contentView.ub_addShadow(radius: 4, opacity: 0.15, xOffset: 0, yOffset: -1)
