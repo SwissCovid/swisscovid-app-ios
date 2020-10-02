7ad7e413c103a04177a212b3fda1bd737d1dc10e
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-10-02 14:50:40 +0200

    removes unneeded error cases

--- DP3TApp/Logic/Tracing/UIState/Errors+Localized.swift
+++ DP3TApp/Logic/Tracing/UIState/Errors+Localized.swift
@@ -35,8 +35,6 @@ extension DP3TTracingError: LocalizedError, CodedError {
             return error.localizedDescription
         case .caseSynchronizationError, .userAlreadyMarkedAsInfected, .cancelled:
             return unexpected.ub_localized
-        case let .databaseError(error):
-            return error?.localizedDescription
         case .bluetoothTurnedOff:
             return "bluetooth_turned_off".ub_localized // custom UI, this should never be visible
         case .permissonError:
@@ -49,8 +47,6 @@ extension DP3TTracingError: LocalizedError, CodedError {
                 return "user_cancelled_key_sharing_error".ub_localized
             }
             return error.localizedDescription
-        case .infectionStatusNotResettable:
-            return nil
         }
     }
 
@@ -60,8 +56,6 @@ extension DP3TTracingError: LocalizedError, CodedError {
             return error.errorCodeString
         case .caseSynchronizationError(errors: _):
             return "ICASYN"
-        case .databaseError(error: _):
-            return "IDBERR"
         case .bluetoothTurnedOff:
             return "IBLOFF"
         case .cancelled:
@@ -75,8 +69,6 @@ extension DP3TTracingError: LocalizedError, CodedError {
         case let .exposureNotificationError(error: error):
             let nsError = error as NSError
             return "IEN\(nsError.code)" // Should match code below
-        case .infectionStatusNotResettable:
-            return "ISNR"
         }
     }
 
@@ -110,7 +102,6 @@ extension DP3TNetworkingError: LocalizedError, CodedError {
         case .noDataReturned: fallthrough
         case .couldNotParseData: fallthrough
         case .couldNotEncodeBody: fallthrough
-        case .batchReleaseTimeMissmatch: fallthrough
         case .jwtSignatureError:
             return "unexpected_error_title".ub_localized
         }
@@ -131,8 +122,6 @@ extension DP3TNetworkingError: LocalizedError, CodedError {
             return "IPARS"
         case .couldNotEncodeBody:
             return "IBODEN"
-        case .batchReleaseTimeMissmatch:
-            return "IBRTMM"
         case .timeInconsistency(shift: _):
             return "ITIMIN"
         case let .jwtSignatureError(code: code, debugDescription: _):
