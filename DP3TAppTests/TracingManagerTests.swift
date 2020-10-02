77cca804f0a11204db759f61f0cf9e4da86475e7
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-10-02 16:52:08 +0200

    fixes unit tests

--- DP3TAppTests/TracingManagerTests.swift
+++ DP3TAppTests/TracingManagerTests.swift
@@ -37,31 +37,21 @@ class MockLocalPush: LocalPushProtocol {
 class TracingManagerTests: XCTestCase {
     func testNotificationScheduling() {
         // since TracingState has no public availble initializer we need to get the object directly from the SDK
-        try! DP3TTracing.reset()
+        DP3TTracing.reset()
 
         let mockLocalPush = MockLocalPush()
         let manager = TracingManager(localPush: mockLocalPush)
 
-        try! DP3TTracing.initialize(with: .init(appId: "xy",
-                                                bucketBaseUrl: URL(string: "https://ubique.ch")!,
-                                                reportBaseUrl: URL(string: "https://ubique.ch")!))
+        DP3TTracing.initialize(with: .init(appId: "xy",
+                                           bucketBaseUrl: URL(string: "https://ubique.ch")!,
+                                           reportBaseUrl: URL(string: "https://ubique.ch")!))
 
-        let ex = expectation(description: "ex")
-        DP3TTracing.status { result in
-            switch result {
-            case .failure:
-                XCTFail()
-            case let .success(state):
-                manager.DP3TTracingStateChanged(state)
-            }
-            ex.fulfill()
-        }
-        wait(for: [ex], timeout: 0.1)
+        manager.DP3TTracingStateChanged(DP3TTracing.status)
 
         XCTAssert(mockLocalPush.scheduleExposureNotificationsIfNeededWasCalled)
 
         XCTAssert(mockLocalPush.handleTracingStateCalled)
 
-        try! DP3TTracing.reset()
+        DP3TTracing.reset()
     }
 }
