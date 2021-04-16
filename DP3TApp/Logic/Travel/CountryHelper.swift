b765743ffcd235f1f3334d4a08b8c5fcf982d483
Fabian Aggeler <aggeler@ubique.ch>
2021-04-16 16:05:32 +0200

    Implement country icon fallback
    Co-authored-by: Konstantin Tabere (tabere@ubique.ch)"

--- DP3TApp/Logic/Travel/CountryHelper.swift
+++ DP3TApp/Logic/Travel/CountryHelper.swift
@@ -13,9 +13,30 @@ import UIKit
 
 final class CountryHelper {
     static func flagForCountryCode(_ code: String) -> UIImage? {
-        let image = UIImage(named: "flag-\(code.lowercased())")
-        image?.accessibilityLabel = Self.localizedNameForCountryCode(code)
-        return image
+        if let image = UIImage(named: "flag-\(code.lowercased())") {
+            image.accessibilityLabel = Self.localizedNameForCountryCode(code)
+            return image
+        } else {
+            // Generate fallback image
+            let label = NSLabel(.ultraSmallBold, textColor: .darkGray, numberOfLines: 1, textAlignment: .center)
+            label.text = code
+            label.layer.borderWidth = 1
+            label.layer.borderColor = UIColor.darkGray.cgColor
+            label.layer.cornerRadius = 3
+            label.backgroundColor = .white
+            label.frame = CGRect(x: 0, y: 0, width: 26, height: 20)
+            label.clipsToBounds = true
+            
+            let renderer = UIGraphicsImageRenderer(size: label.bounds.size)
+            let image = renderer.image { ctx in
+                label.drawHierarchy(in: label.bounds, afterScreenUpdates: true)
+            }
+
+            image.accessibilityLabel = Self.localizedNameForCountryCode(code)
+            return image
+
+        }
+
     }
 
     static func localizedNameForCountryCode(_ code: String) -> String {
