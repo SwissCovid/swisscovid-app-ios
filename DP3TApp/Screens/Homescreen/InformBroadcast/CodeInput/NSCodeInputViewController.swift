ee9d72a1d77c2b52fd0e752b6497b6c35f805ce1
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2021-06-01 09:11:22 +0200

    changed filtering around

--- DP3TApp/Screens/Homescreen/InformBroadcast/CodeInput/NSCodeInputViewController.swift
+++ DP3TApp/Screens/Homescreen/InformBroadcast/CodeInput/NSCodeInputViewController.swift
@@ -156,7 +156,7 @@ class NSCodeInputViewController: NSInformStepViewController, NSCodeControlProtoc
             case let .success(onset):
                 let relevantCheckIns = CheckInManager.shared.getDiary()
                     .filter { $0.checkOutTime != nil && $0.checkOutTime! >= onset.onset }
-                    .filter { $0.venue.venueType != .contactTracingQrCode }
+                    .filter { $0.venue.venueType == .userQrCode }
                 if !ReportingManager.shared.hasUserConsent {
                     ReportingManager.shared.getUserConsent { [weak self] result in
                         guard let self = self else { return }
