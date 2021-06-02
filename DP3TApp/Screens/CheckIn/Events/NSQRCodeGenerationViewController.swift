5e079106fd1d68a5b8605ead68c5fea39fcdd3b1
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2021-06-02 09:32:38 +0200

    limit QR titles to 60 characters

--- DP3TApp/Screens/CheckIn/Events/NSQRCodeGenerationViewController.swift
+++ DP3TApp/Screens/CheckIn/Events/NSQRCodeGenerationViewController.swift
@@ -89,4 +89,14 @@ extension NSQRCodeGenerationViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_: UITextField) -> Bool {
         view.endEditing(true)
     }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+
+        guard let stringRange = Range(range, in: currentText) else { return false }
+
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+
+        return updatedText.count <= 60
+    }
 }
