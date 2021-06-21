d5854791db17d5ba8e2d31b2a5281fab40779635
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2021-06-21 09:18:28 +0200

    changes certificate url scheme and pass qr code information as well

--- DP3TApp/Screens/CheckIn/Scanner/NSCheckInViewController.swift
+++ DP3TApp/Screens/CheckIn/Scanner/NSCheckInViewController.swift
@@ -176,9 +176,9 @@ extension NSCheckInViewController: NSQRScannerViewDelegate {
         // Handle case where user tries to check in a covid certificate
         if str.starts(with: "HC1:") {
             let alert = UIAlertController(title: "", message: "covid_certificate_alert_text".ub_localized, preferredStyle: .alert)
-            if UIApplication.shared.canOpenURL(URL(string: "chcovidcert://")!) {
+            if UIApplication.shared.canOpenURL(URL(string: "covidcert://")!) {
                 alert.addAction(UIAlertAction(title: "covid_certificate_open_app".ub_localized, style: .default, handler: { _ in
-                    UIApplication.shared.open(URL(string: "chcovidcert://")!, options: [:], completionHandler: nil)
+                    UIApplication.shared.open(URL(string: "covidcert://\(str)")!, options: [:], completionHandler: nil)
                 }))
             } else {
                 alert.addAction(UIAlertAction(title: "covid_certificate_install_app".ub_localized, style: .default, handler: { _ in
