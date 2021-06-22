9d5ceb5edff8507e027f6aa8a5ff43b73aec80ba
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2021-06-22 14:18:41 +0200

    remove CE form impressum

--- DP3TApp/Logic/Helpers/Bundle+BuildInfo.swift
+++ DP3TApp/Logic/Helpers/Bundle+BuildInfo.swift
@@ -18,13 +18,4 @@ extension Bundle {
     static var buildNumber: String {
         return Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
     }
-
-    static var buildDate: Date? {
-        let bundleName = Bundle.main.infoDictionary!["CFBundleName"] as? String ?? "Info.plist"
-        if let infoPath = Bundle.main.path(forResource: bundleName, ofType: nil),
-           let infoAttr = try? FileManager.default.attributesOfItem(atPath: infoPath),
-           let infoDate = infoAttr[FileAttributeKey.creationDate] as? Date
-        { return infoDate }
-        return nil
-    }
 }
